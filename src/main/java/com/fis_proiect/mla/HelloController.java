b3b25d319ab08c323a8ff3076f1d453645e0d37d
Its-me-Andreeei <andrei.karolyi@student.upt.ro>
2022-04-29 17:38:24 +0300

    Update on Git basic structure: JavaFX framework added

---
+++ src/main/java/com/fis_proiect/mla/HelloController.java
@@ -0,0 +1,14 @@
+package com.fis_proiect.mla;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.fis_proiect.mla {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.fis_proiect.mla to javafx.fxml;
+    exports com.fis_proiect.mla;
+}
