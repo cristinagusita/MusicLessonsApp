5c0826fd11c88457e79a124805063644f2286fc2
cristinagusita <cristinagusita23@gmail.com>
2022-05-13 10:46:41 +0300

    Project restructure

---
+++ src/main/java/com/example/java11project/Main.java
@@ -0,0 +1,23 @@
+package com.example.java11project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
