9c20327a1ef47e2525a1dc6107f510cca54e1c03
Its-me-Andreeei <102238695+Its-me-Andreeei@users.noreply.github.com>
2022-05-16 22:43:04 +0300

    MLA-43 Code for "Review a teacher" mechanism

--- java11Project/java11Project/src/main/java/com/example/java11project/sample/Controllers/Client/ClientController.java
+++ java11Project/java11Project/src/main/java/com/example/java11project/sample/Controllers/Client/ClientController.java
@@ -50,6 +50,13 @@ public class ClientController {
     }
 
     @FXML
+    public void reviewButtonOnAction(ActionEvent event)throws IOException{
+        AnchorPane pane=FXMLLoader.load(getClass().getClassLoader().getResource("review_client.fxml"));
+        change.getChildren().setAll(pane);
+
+    }
+
+    @FXML
     public void bookButtonOnAction(ActionEvent event)throws IOException{
         AnchorPane pane=FXMLLoader.load(getClass().getClassLoader().getResource("booklesson_client.fxml"));
         change.getChildren().setAll(pane);
