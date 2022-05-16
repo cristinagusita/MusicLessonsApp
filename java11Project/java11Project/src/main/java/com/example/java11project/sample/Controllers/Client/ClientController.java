372f944f51cf813f735bd187c6624b96f5539fda
Its-me-Andreeei <102238695+Its-me-Andreeei@users.noreply.github.com>
2022-05-16 22:57:49 +0300

    MLA-39 Code for "See prices" action

--- java11Project/java11Project/src/main/java/com/example/java11project/sample/Controllers/Client/ClientController.java
+++ java11Project/java11Project/src/main/java/com/example/java11project/sample/Controllers/Client/ClientController.java
@@ -57,6 +57,13 @@ public class ClientController {
     }
 
     @FXML
+    public void seePricesButtonOnAction(ActionEvent event)throws IOException{
+        AnchorPane pane=FXMLLoader.load(getClass().getClassLoader().getResource("prices_client.fxml"));
+        change.getChildren().setAll(pane);
+
+    }
+
+    @FXML
     public void bookButtonOnAction(ActionEvent event)throws IOException{
         AnchorPane pane=FXMLLoader.load(getClass().getClassLoader().getResource("booklesson_client.fxml"));
         change.getChildren().setAll(pane);
